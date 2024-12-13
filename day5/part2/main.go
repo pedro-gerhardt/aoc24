19b742fdb1c612f567c4b8c6375a6d3a1a0edddf
Pedro Gerhardt <pedro.gerhardt@sap.com>
2024-12-13 12:10:17 -0300

    day 5

---
+++ day5/part2/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"aoc24-pleg/utils"
+	"fmt"
+	"os"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
+
+	mapset "github.com/deckarep/golang-set/v2"
+)
+
+func main() {
+	data, err := os.ReadFile("../input.txt")
+	utils.Panic_fn(err)
+
+	rules := make(map[string]mapset.Set[string])
+	isRule := true
+	soma := 0
+	for _, line := range strings.Split(string(data), "\n") {
+		if isRule {
+			if line == "" {
+				isRule = false
+			} else {
+				splitted := strings.Split(line, "|")
+				if rules[splitted[0]] == nil {
+					rules[splitted[0]] = mapset.NewSet[string]()
+				}
+				rules[splitted[0]].Add(splitted[1])
+			}
+		} else {
+			valido := true
+			splitted := strings.Split(line, ",")
+			slices.Reverse(splitted)
+			for a, s := range splitted {
+				for _, n := range splitted[a:] {
+					if rules[s] != nil && rules[s].ContainsOne(n) {
+						valido = false
+					}
+				}
+			}
+			if !valido {
+				sort.Slice(splitted, func(i, j int) bool {
+					return rules[splitted[i]].ContainsOne(splitted[j])
+				})
+				v, err := strconv.Atoi(splitted[len(splitted)/2])
+				utils.Panic_fn(err)
+				soma += v
+			}
+		}
+	}
+
+	fmt.Println(soma)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module aoc24-pleg
 
 go 1.23.3
+
+require github.com/deckarep/golang-set/v2 v2.7.0 // indirect
